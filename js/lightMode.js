54c9bfa759817a387b9a18d689ef6588c6c6fab1
SolovevaJulia <j.soloveva87@yandex.ru>
2021-11-16 18:39:51 +0300

    finished light-mode version

--- js/lightMode.js
+++ js/lightMode.js
@@ -4,3 +4,9 @@ btn.addEventListener('click', function() {
     document.body.classList.add('light-mode');
 });
 
+
+const btnBlack = document.querySelector('.header-cover-button-black');
+
+btnBlack.addEventListener('click', function() {
+    document.body.classList.remove('light-mode');
+});
