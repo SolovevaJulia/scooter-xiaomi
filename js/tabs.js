603c3bd83d489bd16583d6f5bed575d39259d355
SolovevaJulia <j.soloveva87@yandex.ru>
2021-11-22 15:55:29 +0300

    tabs for benefits

---
+++ js/tabs.js
@@ -0,0 +1,17 @@
+let tab = function () {
+    let tabNav = document.querySelectorAll('.header-benefits-card'),
+        tabName;
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav () {
+        tabNav.forEach(item => {
+            item.classList.remove('header-benefits-card-active');
+        });
+        this.classList.add('header-benefits-card-active');
+    }
+};
+
+tab();
